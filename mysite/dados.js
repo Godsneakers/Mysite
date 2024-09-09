9de6239e0c9852b3231be91d299871a606206748
Godsneakers <129785818+Godsneakers@users.noreply.github.com>
2024-09-09 19:58:33 -0300

    Add files via upload

---
+++ mysite/dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Sailor Jerry",
+        descricao: "Sailor Jerry, frequentemente citado como um dos pais da tatuagem americana e é considerado uma figura crucial na evolução do estilo old school.",
+        link: "https://en.wikipedia.org/wiki/Sailor_Jerry"
+    },
+    {
+        titulo: "Filip Leu",
+        descricao: "Considerado um dos maiores nomes do realismo na tatuagem, Filip Leu também é conhecido por suas incríveis trabalhos em pontilhismo. Sua precisão e detalhismo são impressionantes e inspiram muitos tatuadores.",
+        link: "https://de.wikipedia.org/wiki/Filip_Leu"
+    },
+    {
+        titulo: "Thomas Hooper",
+        descricao: "Outro mestre do realismo, Thomas Hooper também domina a técnica do pontilhismo, criando obras de arte realistas e vibrantes na pele.",
+        link: "https://nerdartt.blogspot.com/2013/04/thomas-hooper.html"
+    }
+];
